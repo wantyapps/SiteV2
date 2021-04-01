fc3d3973f5ba4dc33405dd0c9fb70649e8a33734
Uri Arev <uri.arev@gmail.com>
2021-04-01 12:21:45 +0300

    [patch] Adding NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar>
+      <NavbarItem href="#" text="Home"/>
+      <NavbarItem href="#" text="About"/>
+      <NavbarItem href="#" text="Links"/>
+    </Navbar>
   );
 }
 
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-ul">{ props.children }</ul>
+    </nav>
+  );
+}
+
+function NavbarItem(props) {
+  return (
+    <li className="navbar-item">
+      <a href={ props.href } className="navbar-button">{ props.text }</a>
+    </li>
+  );
+}
 export default App;
