dd4fe7bbadee56e3084c24e0ef3aa5ba1f29acbc
Uri Arev <uri.arev@gmail.com>
2021-04-07 21:12:56 +0300

    Remove potential DoS attack vulnerability

--- server.cjs
+++ server.cjs
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const RateLimit = require('express-rate-limit');
+const limiter = new RateLimit({
+  windowMs: 1*60*1000,
+  max: 5
+});
 
+app.use(limiter)
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/api/v1', (req, res) => {
