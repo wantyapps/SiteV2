3c577da7eef5f631d79d5a3941eddc5681880cc7
wantyapps <56798701+wantyapps@users.noreply.github.com>
2021-04-12 16:42:34 +0300

    Refactor, move navbar to Components.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
 import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom"
-import { HomePage, AboutPage, LinksPage, PageNotFound, Navbar, NavbarItem } from "./Components.js"
+import { HomePage, AboutPage, LinksPage, PageNotFound, Navbar, NavbarItem, Footer } from "./Components.js"
 
 function App() {
   return (
     <div>
-      <Navbar>
-        <NavbarItem href="/home" text="Home"/>
-        <NavbarItem href="/about" text="About"/>
-        <NavbarItem href="/links" text="Links"/>
-        <NavbarItem href="/api/v1/doc" text="Developer"/>
-      </Navbar>
       <BrowserRouter>
         <Switch>
           <Redirect exact from="/" to="/home"/>
@@ -20,9 +14,7 @@ function App() {
           <Route path="*" component={PageNotFound}/>
         </Switch>
       </BrowserRouter>
-      <footer className="footer">
-        <p>Created by Uri Arev (2021). Copyright (©) Uri Arev 2021</p>
-      </footer>
+      <Footer/>
     </div>
   );
 }
