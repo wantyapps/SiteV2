087da49a8dec12e714bc0adab13a875981b6e7e7
wantyapps <56798701+wantyapps@users.noreply.github.com>
2021-04-12 11:29:02 +0300

    [minor] V0.1.0 is out!

--- src/Components.js
+++ src/Components.js
@@ -2,6 +2,10 @@ export function HomePage() {
     return (
         <center>
             <h1>I build software.</h1>
+            <h2>Software?</h2>
+            <ul class="SoftwaresUL">
+                <li><a href="https://github.com/wantyapps/SiteV2">This site!</a></li>
+            </ul>
         </center>
     );
 }
@@ -10,6 +14,10 @@ export function AboutPage() {
     return (
       <center>
           <h1>About</h1>
+          <p>Hello! My name is Uri Arev (aka. Wantyapps), and I love building software.</p>
+          <p>My mission is to create software that helps developers, and to</p>
+          <p>just make people happy.</p>
+          <h4>That's me!</h4>
       </center>
     );
 }
@@ -18,6 +26,11 @@ export function LinksPage() {
     return (
         <center>
             <h1>Links</h1>
+            <ul class="LinksUL">
+                <li><a href="https://github.com/Official-Wantyapps/">The Official Wantyapps GitHub Organization</a></li>
+                <li><a href="https://github.com/wantyapps/">My GitHub</a></li>
+                <li><a href="https://gitlab.com/wantyapps/">My GitLab</a></li>
+            </ul>
         </center>
     )
 }
@@ -25,7 +38,7 @@ export function LinksPage() {
 export function PageNotFound() {
     return (
         <center>
-            <h1>Ouch...</h1>
+            <h1>Ouch... Page not found.</h1>
         </center>
     )
 }
