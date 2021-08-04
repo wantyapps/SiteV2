60e9d70612c91977bb90313b41dbffc27753258b
wantyapps <56798701+wantyapps@users.noreply.github.com>
2021-08-05 02:19:20 +0300

    Refactor: Add `resources` folder organize stuff

--- resources/api/app.js
+++ resources/api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const joke = require("./joke.js");
 const app = express();
 
 dotenv.config()
@@ -11,12 +12,6 @@ if ( process.argv[2] === "d" || process.argv[2] === "debug" ) {
 	console.log("Debug mode enabled.");
 };
 
-function tellJoke() {
-	const jokes = ["Two guys are busted stealing a calender. The both got 6 months", "Who knows about math and science and is always close by? An engi-near!", "What is a cat's favorite website? Reddot.com", "Why did the woman fall into the well? She couldn't see that well.", "Why didn't the chicken cross the road? Because It was eggshausted."];
-	let randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
-	return randomJoke;
-}
-
 app.get('/api/v1', (req, res) => {
 	if ( req.headers.username === process.env.WANTYAPIUSERNAME && req.headers.password == process.env.WANTYAPIPASSWORD) {
 		if ( ProcessDebug === true ) {
@@ -24,7 +19,7 @@ app.get('/api/v1', (req, res) => {
 		};
 		var toSend = {"success": true, "error": false};
 		if ( req.headers.action === "joke" ) {
-			toSend["joke"] = tellJoke();
+			toSend["joke"] = joke.tellJoke();
 		};
 		res.send(toSend);	
 	} else {
---
+++ resources/api/joke.js
@@ -0,0 +1,5 @@
+exports.tellJoke = function () {
+	const jokes = ["Two guys are busted stealing a calender. The both got 6 months", "Who knows about math and science and is always close by? An engi-near!", "What is a cat's favorite website? Reddot.com", "Why did the woman fall into the well? She couldn't see that well.", "Why didn't the chicken cross the road? Because It was eggshausted."];
+	let randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+	return randomJoke;
+}
