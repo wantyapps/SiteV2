d61b91c13656948cae58fe4980704cc04348a573
wantyapps <56798701+wantyapps@users.noreply.github.com>
2021-08-18 23:38:23 +0300

    Add Dark_Light template

---
+++ src/components/Dark_Light.js
@@ -0,0 +1,7 @@
+import "./css/Dark_Light.css";
+
+export function Dark_Light() {
+    return (
+        <button></button>
+    );
+}
---
+++ src/components/test/Dark_Light.test.js
@@ -0,0 +1,11 @@
+// TODO: Add tests for this component
+
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Dark_Light } from "../Dark_Light.js";
+
+describe("Dark_Light", () => {
+    it('renders the Dark/Light mode button correctly', () => {
+        // TODO: Add test here
+    });
+});
