5bc7b331bbc1f3ab2c323ed59b8049cd9051e8d1
wantyapps <56798701+wantyapps@users.noreply.github.com>
2022-01-22 02:58:56 +0200

    wtf is wrong with jest

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-    presets: [
-        "@babel/preset-react",
-        "@babel/preset-env"
-    ],
-    "plugins": [
-        "react-require"
-    ]
+	presets: [
+		"@babel/preset-react",
+		"@babel/preset-env"
+	],
+	"plugins": [
+		"react-require"
+	]
 }
